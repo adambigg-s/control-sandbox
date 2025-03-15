29c27ec2afd5fa670d7d28dea9fccfd636e025ae
adambigg-s <biggsa601@gmail.com>
2025-03-14 23:46:02 -0400

    demo

--- controller.c
+++ controller.c
@@ -1,13 +1,33 @@
 
 typedef struct State {
-    
+    float x, y, z;
+    float phi, theta, psi;
+    float u, v, w;
+    float p, q, r;
 } State;
 
 typedef struct Control {
-    
+    float aileron;
+    float elevator;
+    float rudder;
+    float throttle;
 } Control;
 
+void clamp(float* target, float min, float max) {
+    if (*target < min) {
+        *target = min;
+    }
+    if (*target > max) {
+        *target = max;
+    }
+}
+
 Control controller(const State *state) {
-    Control output = {};
-    return output;
+    Control controller = {
+        .aileron = 0.,
+        .elevator = -0.1,
+        .rudder = 0.,
+        .throttle = 0.5,
+    };
+    return controller;
 }
